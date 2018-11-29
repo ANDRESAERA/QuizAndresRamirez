d4ff3c9decdb1ff2675e50f035e00e3f9a06d2b2
alumno Secundaria <al3201@cedi.local>
2018-11-29 08:37:44 -0600

    quiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RegistroPage } from '../pages/registro/registro';
+import { LoginPage } from '../pages/login/login';
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    RegistroPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    RegistroPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/registro/registro.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Storage} from "@ionic/storage";
+
+
+@IonicPage()
+@Component({
+  selector: 'page-registro',
+  templateUrl: 'registro.html',
+})
+export class RegistroPage {
+  correo = '';
+  contra = '';
+  usuarios = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+    this.usuarios = this.navParams.get('usuarios');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegistroPage');
+  }
+
+  clickRegistro() {
+    console.log(this.correo);
+    console.log(this.contra);
+    this.usuarios.push({
+      correo: this.correo,
+      contra: this.contra
+    });
+    // modicar la llave para tener diferentes usuarios
+    this.storage.set('usuarios', JSON.stringify(this.usuarios));
+    this.navCtrl.pop();
+  }
+
+}
